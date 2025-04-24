e6a269bdf89992ceefe1bf7d5344da310daa563a
Kevin-Zalavadiya <kevinzalavadiya9898@gmail.com>
2025-04-24 07:52:06 +0530

    Initial commit

--- rai/templates/finaltest_viewinvoice.js
+++
@@ -1,7 +0,0 @@
-function viewInvoice() {
-    const totalPrice = document.getElementById('total-price').innerText;
-    // Store the total price in localStorage so we can access it on the invoice page
-    localStorage.setItem('invoiceTotal', totalPrice);
-    // Redirect to the invoice page
-    window.location.href = invoiceUrl;
-}
